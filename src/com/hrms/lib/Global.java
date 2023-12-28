a940c67141453eb6526fe808e241900006186642
GOPAL <GOPAL@DESKTOP-QLIAE5G>
2023-12-28 16:36:57 +0530

    hiii

---
+++ src/com/hrms/lib/Global.java
@@ -0,0 +1,25 @@
+package com.hrms.lib;
+
+import java.util.Scanner;
+
+public class Global {
+public WebDriver driver;
+Object s=new Scanner(System.in);
+public String url ="http://183.82.103.245/nareshit/login.php";
+public String un="nareshit";
+public String pw="nareshit";
+public String efn="selenium";
+public String eln="suresh";
+public String txt_loginname="txtUserName";
+public String txt_password="txtPassword";
+public String btn_login="submit";
+public String link_logout="Logout";
+public String frame_emoloyeeinfo="rightMenu";
+public String btn_Add="//input[@value='Add'];"
+public String txt_efn="txtEmpFirstName";
+public String txt_eln="txtEmpLastName";
+public String btn_Save="btnEdit";
+}
+
+
+
